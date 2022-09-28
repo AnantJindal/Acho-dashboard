1885fa26963ea9a07b4bb7503824a7b06b220031
Anant Jindal <anantjindal@sketchbrahma.com>
2022-09-28 22:42:09 +0530

    Table Part is remaining in responsive

---
+++ assests/scripts/script.js
@@ -0,0 +1,9 @@
+
+function openSideBar() {
+    const element = document.getElementById("mobileSideBar");
+    element.classList.add("open");
+  }
+  function closeSideBar() {
+    const element = document.getElementById("mobileSideBar");
+    element.classList.remove("open");
+  }
